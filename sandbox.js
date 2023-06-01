33aceb32184d65d91686d45c14ef7bfab4e15d43
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-06-01 21:33:57 +0800

    basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,34 @@
+// Basics
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+// Arrow function
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(30));
+// Arrays
+var names = ['john', 'rino', 'joseph'];
+names.push('new');
+var numbers = [10, 32, 34, 43];
+numbers.push(45);
+var mixed = ['sandy', 43, 'linda', 7, 12];
+mixed.push('filo');
+mixed.push(34);
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'red',
+    age: 32
+};
+ninja.age = 40;
+ninja.name = 'vin';
+ninja = {
+    name: 'yoshe',
+    belt: 'black',
+    age: 99
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,46 @@
-const character = 'mario';
+// Basics
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
 
-const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
+// Arrow function
+const circ = (diameter) => {
+    return diameter * Math.PI;
+}
 
-inputs.forEach(input => {
-  console.log(input);
-});
+console.log(circ(30)); 
+
+
+// Arrays
+let names = ['john', 'rino', 'joseph'];
+names.push('new');
+
+let numbers = [10, 32, 34, 43];
+numbers.push(45);
+
+let mixed = ['sandy', 43, 'linda', 7, 12];
+mixed.push('filo');
+mixed.push(34);
+mixed[0] = 3;
+
+
+// Objects
+let ninja = {
+    name: 'mario',
+    belt: 'red',
+    age: 32
+};
+
+ninja.age = 40;
+ninja.name = 'vin';
+
+ninja = {
+    name: 'yoshe',
+    belt: 'black',
+    age: 99
+}
