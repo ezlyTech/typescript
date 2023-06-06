a3f09bfdf868ac1a2082a32a38201e0f4f885624
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-06-06 21:50:55 +0800

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+var me = {
+    name: 'shaun',
+    age: 23,
+    speak: function (text) {
+        console.log(text);
+    },
+    spend: function (amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+var greetPerson = function (person) {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/Invoice.js';
 var invOne = new Invoice('mario', 'work on the mario website', 250);
 var invTwo = new Invoice('luisa', 'work on the luisa website', 300);
