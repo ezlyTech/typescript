d1416d9700723d2cbdad829aedec4cca0afcb701
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-06-06 17:44:43 +0800

    type aliases

---
+++ public/type-aliases.js
@@ -0,0 +1,10 @@
+"use strict";
+var logDetails = function (uid, item) {
+    console.log("".concat(item, " has a uid of ").concat(uid));
+};
+var greeting = function (user) {
+    console.log("".concat(user.name, " says hello!"));
+};
+var greetingAgain = function (user) {
+    console.log("".concat(user.name, " says hello!"));
+};
