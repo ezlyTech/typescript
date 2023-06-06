e177565126d7832c2adc16f1afb0f307ab04637b
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-06-06 20:07:27 +0800

    classifiers

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ) {}
 
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -22,11 +22,11 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'yoshi';
-invTwo.amount = 12;
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 
-console.log(invOne, invTwo);
-console.log(invoices);
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
